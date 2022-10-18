68e0d08110d33b146b481b19e5402ac68bce3d0d
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-10-18 06:02:05 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * Main - print _putchar
+ *
+ * Description: print _putchar
+ *
+ * Return (0) succes
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	putchar(10);
+	return (0);
+}
