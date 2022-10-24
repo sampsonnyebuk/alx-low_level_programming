198acb62a01e6b90742c39694add2c0ab4451cdb
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-10-24 09:01:09 -0700

    arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array
@@ -19,10 +20,10 @@ void print_array(int *a, int n)
 	{
 		while (a[i] != '\0' && count < n)
 		{
-			printf("%d, ", a[i]);
+			_putchar("%d, ", a[i]);
 			i++;
 			count++;
 		}
-		printf("%d\n", a[i]);
+		_putchar("%d\n", a[i]);
 	}
 }
