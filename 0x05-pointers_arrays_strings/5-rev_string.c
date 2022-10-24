3f9f113662d72529385a1fcfbeb606b2a61e112b
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-10-24 08:21:53 -0700

    revers strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,24 +2,29 @@
 #include <stdio.h>
 
 /**
- * print_rev - prints string backwards
+ * rev_string - reverse string
  * @s: string
  * Return: void
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
-	int i = 0;
+	int i, max, half;
+	char first, last;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (i >= 0)
+	max = i - 1;
+	lf = max / 2;
+	while (half >= 0)
 	{
-		_putchar(s[i]);
-		i--;
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
 	}
-	_putchar('\n');
 }
