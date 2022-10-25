42071ce35d88f4fd79b0bda8b9c1406a4af922a6
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-10-25 13:19:31 -0700

    reverse the string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,13 +18,13 @@ void rev_string(char *s)
 		i++;
 	}
 	max = i - 1;
-	half = max / 2;
+	lf = max / 2;
 	while (half >= 0)
 	{
 		first = s[max - half];
 		last = s[half];
 		s[half] = first;
 		s[max - half] = last;
-		half--
+		half--;
 	}
 }
