87ef0909262fd5e917e16833fb4487b577a0b7c4
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-10-26 13:09:30 -0700

    header and main work

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @p: The string will be modified
+ *
+ * Return: char var
+ */
+
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i])
+	{
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[i] -= 32;
+		}
+
+		i++;
+	}
+
+	return (p);
+}
