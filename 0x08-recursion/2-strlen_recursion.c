abe8a8129b8baab2a1c8410e9b8afaea17ee3c59
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-11-02 13:56:15 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - works out the length of the line
+ * @s: the string varible to count
+ * Return: integer value
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * factorial - gets the factorial of a number
+ * @n:The parameter containing the number
+ * Return: integer value
+ */
+
+int factorial(int n)
+{
+	int result;
+
+	if (n == 0)
+	{
+		result = 1;
+		return (result);
+	}
+
+	else if (n < 0)
+	{
+		result = -1;
+		return (result);
+	}
+
+	else
+	{
+		result = n * factorial(n - 1);
+		return (result);
+	}
+}
