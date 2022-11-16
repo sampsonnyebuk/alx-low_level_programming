4823227eb92f3e060164d1a3d53ae05d1ac6777b
Nyebuk Sampson Inyang <inyangnyebuk@gmail.com>
2022-11-16 13:32:11 -0800

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - The iterator function
+ * @array: The array
+ * @size: The array size
+ * @action: The function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (size > 0 && array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i = i + 1;
+		}
+	}
+}
